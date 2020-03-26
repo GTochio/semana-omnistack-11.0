cae9b57488e9c57c07db378d80d9d73651cd8ecd
Gabriel <tgabrielfernando@gmail.com>
2020-03-26 00:48:18 -0300

    Back-End

--- Backend/src/controllers/SessionController.js
+++ Backend/src/controllers/SessionController.js
@@ -10,7 +10,7 @@ module.exports = {
         .first();
 
         if(!ong){
-            return response.status(400).send({error: 'No ONG found with this ID' })
+            return response.status(400).send({error: 'No ONG found with this ID' });
 
         }
         return response.json(ong)
